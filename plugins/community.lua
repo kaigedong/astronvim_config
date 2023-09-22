18751bc7af48d56e2cfe872fcdc166d35e640fca
kaigedong <dongkaige@gmail.com>
2023-09-22 10:37:28 +0800

    fix: large file lsp works

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,13 +1,14 @@
-return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
+return { -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity", -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+
   { import = "astrocommunity.file-explorer.mini-files" },
   { import = "astrocommunity.git.git-blame-nvim" },
 
@@ -19,6 +20,4 @@ return {
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.lua" },
-
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
 }
